0d43150f83659f9f877cdae506d3ac77218a139e
Dayesh <dayesh1996@gmail.com>
2024-09-01 22:27:11 +0530

    first commit

---
+++ myproject/myapp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import generics
+from .models import Item
+from .serializers import ItemSerializer
+
+class ItemListCreateView(generics.ListCreateAPIView):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+    
